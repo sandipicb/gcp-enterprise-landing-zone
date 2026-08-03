5db647780d15136853651820ef2997c4229f76d2
Sandip <sandipicb@gmail.com>
2026-08-03 14:26:56 +0600

    Add Cloud Monitoring dashboard, alerts, and log-based metrics

--- terraform/modules/monitoring/outputs.tf
+++ terraform/modules/monitoring/outputs.tf
@@ -1,3 +1,16 @@
 output "notification_channel" {
-    value = google_monitoring_notification_channel.email.id
+  value = google_monitoring_notification_channel.email.id
+}
+
+output "alert_policy" {
+  value = google_monitoring_alert_policy.high_cpu.id
+}
+
+output "dashboard" {
+  value = google_monitoring_dashboard.landing_zone_dashboard.id
+}
+
+output "log_metric_name" {
+  description = "Name of the log-based metric"
+  value       = google_logging_metric.vm_errors.name
 }
